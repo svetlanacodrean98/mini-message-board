056240c3383ee4134da27ecb09c828cbc104d479
Svetlana Codrean <codrean.sveta@gmail.com>
2026-02-06 15:11:52 +0100

    add link to new message page

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,11 +2,11 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
-const messages = [
-];
+const messages = [];
+const link = { href: "new", text: "New Message" };
 
 indexRouter.get("/", (req, res) => {
-    res.render("index", { title: "Mini Messageboard", messages: messages });
+    res.render("index", { title: "Mini Messageboard", messages: messages, link: link });
 });
 indexRouter.post("/", (req, res) => {
     messages.push({text: req.body.message, user: req.body.name, added: new Date()});
